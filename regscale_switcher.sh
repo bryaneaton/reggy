1c5de3b26ad3a61f4d869f0d60d099d663f8c761
Bryan Eaton <bryaneaton77@gmail.com>
2023-11-17 08:44:34 -0500

    init

---
+++ regscale_switcher.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env fish
+
+cd /opt/domain_switcher || exit
+export REGSCALE_DOMAIN=$( rofi -normal-window -steal-focus -dmenu -p "Select RegScale Domain environment" < domains)
+printf "REGSCALE_DOMAIN changed to %s" "$REGSCALE_DOMAIN"
+cd - || exit
